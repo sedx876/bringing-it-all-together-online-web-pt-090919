72929b4331713c997a267dff96a5b0a39f02c7e8
Sharon Watkins <swatkins876@gmail.com>
2019-11-12 11:43:48 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -64,7 +64,7 @@ def self.drop_table
     dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
     if !dog.empty?
       dog_data = dog[0]
-      dog = DOG.new(dog_data[0], dog_data[1], dog_data[2])
+      dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
     else
       dog = self.create(name: name, breed: breed)
     end
