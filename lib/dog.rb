9f59e223838327f7d02d66b45a91021ca91088aa
Sharon Watkins <swatkins876@gmail.com>
2019-11-12 11:34:34 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -49,9 +49,11 @@ def self.drop_table
   end
 
   def self.find_by_id(id)
-    sql = "SELECT * FROM dogs WHERE id = ?"
-    result = DB[:conn].execute(sql, id)[0]
-    Dog.new(result[0], result[1], result[2])
-  end
+    sql = <<-SQL 
+      SELECT * 
+      FROM dogs
+      WHERE id = ?
+      LIMIT 1 
+    SQL
 
 end
