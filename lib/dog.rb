8166f6e14cf5e0e8e0bd2d8eccea9fb38e7729b8
Sharon Watkins <swatkins876@gmail.com>
2019-11-12 11:26:45 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -24,13 +24,20 @@ def self.drop_table
     DB[:conn].execute(sql)
   end
 
-  def save
-    sql = <<-SQL
-      INSERT INTO dogs (name, breed, id)
-      VALUES (?, ?)
-    SQL
+  def save 
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO dogs (name, breed)
+        VALUES (?, ?)
+      SQL
 
-    DB[:conn].execute(sql, self.name, self.breed, self.id)
+      DB[:conn].execute(sql, self.name, self.breed)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+
+      self
+    end
   end
 
 end
